155e4d38868dcd0edaeaa3fdf361b4a871555fd5
logashprabu <30844280+logashprabu@users.noreply.github.com>
2024-10-29 20:29:20 +0530

    Update sign_pdf.py

--- sign_pdf.py
+++ sign_pdf.py
@@ -1,58 +1,67 @@
 import os
 import time
 import argparse
+import json
 from PDFNetPython3.PDFNetPython import *
-from typing import Tuple
+from typing import Tuple, List
 
 # Declare the variable for the PKCS#12 file path
 pk_filename = "certificate.pfx"  # Replace with your actual path
 
 def sign_file(input_file: str, signatureID: str, x_coordinate: int, 
-            y_coordinate: int, pages: Tuple = None, output_file: str = None
-              ):
+               y_coordinate: int, pages: List[int] = None, output_file: str = None):
     """Sign a PDF file"""
     # An output file is automatically generated with the word signed added at its end
     if not output_file:
-        output_file = (os.path.splitext(input_file)[0]) + "_signed.pdf"
+        output_file = os.path.splitext(input_file)[0] + "_signed.pdf"
+    
     # Initialize the library
     PDFNet.Initialize()
     doc = PDFDoc(input_file)
+    
     # Create a signature field
     sigField = SignatureWidget.Create(doc, Rect(
-        x_coordinate, y_coordinate, x_coordinate+100, y_coordinate+50), signatureID)
+        x_coordinate, y_coordinate, x_coordinate + 100, y_coordinate + 50), signatureID)
+
     # Iterate throughout document pages
-    for page in range(1, (doc.GetPageCount() + 1)):
+    for page in range(1, doc.GetPageCount() + 1):
         # If required for specific pages
-        if pages:
-            if str(page) not in pages:
-                continue
+        if pages and page not in pages:
+            continue
         pg = doc.GetPage(page)
         # Create a signature text field and push it on the page
         pg.AnnotPushBack(sigField)
+
     # Signature image
-    sign_filename = os.path.dirname(
-        os.path.abspath(__file__)) + "\static\signature.jpg"
-    # Retrieve the signature field.
+    sign_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static", "signature.jpg")
+    
+    # Retrieve the signature field
     approval_field = doc.GetField(signatureID)
     approval_signature_digsig_field = DigitalSignatureField(approval_field)
-    # Add appearance to the signature field.
+
+    # Add appearance to the signature field
     img = Image.Create(doc.GetSDFDoc(), sign_filename)
-    found_approval_signature_widget = SignatureWidget(
-        approval_field.GetSDFObj())
+    found_approval_signature_widget = SignatureWidget(approval_field.GetSDFObj())
     found_approval_signature_widget.CreateSignatureAppearance(img)
-    # Prepare the signature and signature handler for signing.
+
+    # Prepare the signature and signature handler for signing
     approval_signature_digsig_field.SignOnNextSave(pk_filename, "TerosonIsON0523!")
-    # The signing will be done during the following incremental save operation.
+    
+    # The signing will be done during the following incremental save operation
     doc.Save(output_file, SDFDoc.e_incremental)
+    
     # Develop a Process Summary
     summary = {
-        "Input File": input_file, "Signature ID": signatureID, 
-        "Output File": output_file, "Signature File": sign_filename, 
+        "Input File": input_file, 
+        "Signature ID": signatureID, 
+        "Output File": output_file, 
+        "Signature File": sign_filename, 
         "Certificate File": pk_filename
     }
+    
     # Printing Summary
     print("## Summary ########################################################")
-    print("\n".join("{}:{}".format(i, j) for i, j in summary.items()))
+    print("\n".join("{}: {}".format(i, j) for i, j in summary.items()))
     print("###################################################################")
     return True
 
@@ -62,11 +71,11 @@ def sign_folder(**kwargs):
     input_folder = kwargs.get('input_folder')
     signatureID = kwargs.get('signatureID')
     pages = kwargs.get('pages')
-    x_coordinate = int(kwargs.get('x_coordinate'))
-    y_coordinate = int(kwargs.get('y_coordinate'))
-    # Run in recursive mode
+    x_coordinate = kwargs.get('x_coordinate')
+    y_coordinate = kwargs.get('y_coordinate')
     recursive = kwargs.get('recursive')
-    # Loop though the files within the input folder.
+
+    # Loop through the files within the input folder
     for foldername, dirs, filenames in os.walk(input_folder):
         for filename in filenames:
             # Check if pdf file
@@ -75,7 +84,7 @@ def sign_folder(**kwargs):
             # PDF File found
             inp_pdf_file = os.path.join(foldername, filename)
             print("Processing file =", inp_pdf_file)
-            # Compress Existing file
+            # Sign the PDF file
             sign_file(input_file=inp_pdf_file, signatureID=signatureID, x_coordinate=x_coordinate,
                       y_coordinate=y_coordinate, pages=pages, output_file=None)
         if not recursive:
@@ -85,7 +94,7 @@ def sign_folder(**kwargs):
 def is_valid_path(path):
     """Validates the path inputted and checks whether it is a file path or a folder path"""
     if not path:
-        raise ValueError(f"Invalid Path")
+        raise ValueError("Invalid Path")
     
     # Normalize the path to avoid issues with double slashes
     normalized_path = os.path.normpath(path)
@@ -95,8 +104,7 @@ def is_valid_path(path):
     elif os.path.isdir(normalized_path):
         return normalized_path
     else:
-        raise ValueError(f"Invalid Path {path}")
-
+        raise ValueError(f"Invalid Path: {path}")
 
 
 def parse_args():
@@ -106,12 +114,13 @@ def parse_args():
                         help="Enter the path of the file or the folder to process")
     parser.add_argument('-s', '--signatureID', dest='signatureID',
                         type=str, help="Enter the ID of the signature")
-    parser.add_argument('-p', '--pages', dest='pages', type=tuple,
+    parser.add_argument('-p', '--pages', dest='pages', type=str,
                         help="Enter the pages to consider e.g.: [1,3]")
     parser.add_argument('-x', '--x_coordinate', dest='x_coordinate',
                         type=int, help="Enter the x coordinate.")
     parser.add_argument('-y', '--y_coordinate', dest='y_coordinate',
                         type=int, help="Enter the y coordinate.")
+    
     path = parser.parse_known_args()[0].input_path
     if path and os.path.isfile(path):
         parser.add_argument('-o', '--output_file', dest='output_file',
@@ -119,10 +128,16 @@ def parse_args():
     if path and os.path.isdir(path):
         parser.add_argument('-r', '--recursive', dest='recursive', default=False, type=lambda x: (
             str(x).lower() in ['true', '1', 'yes']), help="Process Recursively or Non-Recursively")
+
     args = vars(parser.parse_args())
+
+    # Convert pages from string to list of integers
+    if args['pages']:
+        args['pages'] = json.loads(args['pages'])
+
     # To Display The Command Line Arguments
     print("## Command Arguments #################################################")
-    print("\n".join("{}:{}".format(i, j) for i, j in args.items()))
+    print("\n".join("{}: {}".format(i, j) for i, j in args.items()))
     print("######################################################################")
     return args
 
@@ -135,7 +150,7 @@ if __name__ == '__main__':
     if os.path.isfile(args['input_path']):
         sign_file(
             input_file=args['input_path'], signatureID=args['signatureID'],
-            x_coordinate=int(args['x_coordinate']), y_coordinate=int(args['y_coordinate']), 
+            x_coordinate=args['x_coordinate'], y_coordinate=args['y_coordinate'], 
             pages=args['pages'], output_file=args['output_file']
         )
     # If Folder Path
@@ -143,6 +158,6 @@ if __name__ == '__main__':
         # Process a folder
         sign_folder(
             input_folder=args['input_path'], signatureID=args['signatureID'], 
-            x_coordinate=int(args['x_coordinate']), y_coordinate=int(args['y_coordinate']),
+            x_coordinate=args['x_coordinate'], y_coordinate=args['y_coordinate'],
             pages=args['pages'], recursive=args['recursive']
         )
